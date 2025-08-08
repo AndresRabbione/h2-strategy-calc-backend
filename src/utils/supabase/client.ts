d0649a84af2e5aeaa52c01529a510d8224140de0
AndresRabbione <168472658+AndresRabbione@users.noreply.github.com>
2025-08-08 13:28:07 -0300

    Fixing logging

--- src/utils/supabase/client.ts
+++ src/utils/supabase/client.ts
@@ -1,8 +1,5 @@
 import { createBrowserClient } from "@supabase/ssr";
 
 export function createClient() {
-  console.log("URL: ", process.env.SUPABASE_URL);
-  console.log("KEY: ", process.env.ANON_KEY);
-
   return createBrowserClient(process.env.SUPABASE_URL!, process.env.ANON_KEY!);
 }
