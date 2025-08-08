8425de2ffe16cbdf9f567232af061fd2dacdcb2b
AndresRabbione <168472658+AndresRabbione@users.noreply.github.com>
2025-08-08 13:35:09 -0300

    Fixing environment

--- src/utils/supabase/client.ts
+++ src/utils/supabase/client.ts
@@ -1,5 +1,8 @@
 import { createBrowserClient } from "@supabase/ssr";
 
 export function createClient() {
-  return createBrowserClient(process.env.SUPABASE_URL!, process.env.ANON_KEY!);
+  return createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_ANON_KEY!
+  );
 }
--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -7,8 +7,8 @@ export async function updateSession(request: NextRequest) {
   });
 
   const supabase = createServerClient(
-    process.env.SUPABASE_URL!,
-    process.env.ANON_KEY!,
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_ANON_KEY!,
     {
       cookies: {
         getAll() {
