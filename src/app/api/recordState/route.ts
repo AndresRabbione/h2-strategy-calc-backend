d3d77ae2f40d095fe4cf98209c1bb02bb0805672
AndresRabbione <168472658+AndresRabbione@users.noreply.github.com>
2025-09-22 15:35:47 -0300

    Adding first automation for recording data

---
+++ src/app/api/recordState/route.ts
@@ -0,0 +1,31 @@
+import { NextResponse } from "next/server";
+import { createClient } from "@supabase/supabase-js";
+import { recordCurrentState } from "@/utils/strategies/recording";
+
+export async function GET(requuest: Request) {
+  const auth = requuest.headers.get("Authorization");
+
+  if (auth !== `Bearer ${process.env.CRON_SECRET}`) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  }
+
+  const supabase = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.SUPABASE_SERVICE_ROLE_KEY!
+  );
+
+  try {
+    const response = await recordCurrentState(supabase);
+
+    if (!response) {
+      return NextResponse.json(
+        { error: "Failed to record current state" },
+        { status: 500 }
+      );
+    }
+
+    return NextResponse.json({ success: true }, { status: 200 });
+  } catch (err: any) {
+    return NextResponse.json({ error: err.message }, { status: 500 });
+  }
+}
